58f1656fd5b303067044f0aa2e34b30b0fd2f33f
mnsalimi <sartakhti.salimi@gmail.com>
2022-04-21 01:53:12 +0430

    fixed login page problem

---
+++ tools/dev-nuxt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+root="$(dirname "$0")/.."
+app="${root}/frontend"
+
+(
+  cd "${app}"
+ 
+  if [[ ! -d node_modules/.bin ]]; then
+    echo "Installing dependencies"
+    yarn install
+  fi
+
+  echo "Starting frontend server"
+  yarn lintfix
+  yarn dev
+)
